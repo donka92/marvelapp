6644bdfb7945b5c2cab1ca476412d400b667043c
donka92 <donkadraganova92@gmail.com>
2019-08-05 02:10:07 +0300

    deploy to heroku

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 //Install express server
 const express = require('express');
 const path = require('path');
-
+var port = process.env.PORT || 8080;
 const app = express();
 
 // Serve only the static files form the dist directory
@@ -9,8 +9,14 @@ app.use(express.static(__dirname + '/dist'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/index.html'));
+      res.sendFile(path.join(__dirname+'/dist/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080)
+app.listen(port, (err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(`server started port: ${port}`);
+    }
+  });
