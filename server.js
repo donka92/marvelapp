55a8e5e802e5114e45905a3b6c86a7642b551d2f
donka92 <donkadraganova92@gmail.com>
2019-08-05 08:50:04 +0300

    heroku commit

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 //app.use(express.static(__dirname + '/dist'));
 //app.use(express.static('www'));
 
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist/demomarvelapp/index.html'));
 
 //app.get('/*', function(req,res) {
     
