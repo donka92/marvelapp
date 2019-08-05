98186bbea042c2e06712f4ef6814875da176cbfa
donka92 <donkadraganova92@gmail.com>
2019-08-05 08:54:27 +0300

    heroku commit

--- server.js
+++ server.js
@@ -6,9 +6,7 @@ const app = express();
 
 // Serve only the static files form the dist directory
 //app.use(express.static(__dirname + '/dist'));
-//app.use(express.static('www'));
-
-app.use(express.static(__dirname + '/dist/demomarvelapp/index.html'));
+app.use(express.static('www'));
 
 //app.get('/*', function(req,res) {
     
