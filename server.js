05e3241a63ad99b7a6d461c3c7edc4e6c24f311d
donka92 <donkadraganova92@gmail.com>
2019-08-05 07:20:57 +0300

    heroku commit

--- server.js
+++ server.js
@@ -5,12 +5,13 @@ var port = process.env.PORT || 8080;
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist'));
+//app.use(express.static(__dirname + '/dist'));
+app.use(express.static('www'));
 
-app.get('/*', function(req,res) {
+//app.get('/*', function(req,res) {
     
-      res.sendFile(path.join(__dirname+'/dist/index.html'));
-});
+ //     res.sendFile(path.join(__dirname+'/dist/index.html'));
+//});
 
 // Start the app by listening on the default Heroku port
 app.listen(port, (err) => {
